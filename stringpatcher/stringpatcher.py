3fd5d82ed932f3da487e286433b0a8ec49adcf02
ivo <ipooters@gmail.com>
2015-06-04 12:19:21 +0200

    fixed bugs

--- stringpatcher/stringpatcher.py
+++ stringpatcher/stringpatcher.py
@@ -33,25 +33,34 @@ def show(filepath, enc):
 @click.option("--enc", "-e", default="l", type=click.Choice(["s", "S", "b", "l", "B", "L"]), help="Select character size and endianness: s = 7-bit, S = 8-bit, {b,l} = 16-bit, {B,L} = 32-bit")
 @click.argument("filepath", type=click.Path(writable=True))
 def patch(filepath, enc):
+    p_enc = encmap[enc]
     """ Patch the strings in file. 
 Reads list of '<offset> <string>' entry from stdin """
-    with open(filepath, "bw") as f:
-        entries = sys.stdin.read().split("\n")
+    with open(filepath, "r+b") as f:
+        entries = sys.stdin.read().splitlines(False)
         click.echo("%d entries"%len(entries))
         for entry in entries:
             if not entry or entry.startswith("#"):
                 continue
-            (offset, rplstr) = entry.strip().split(maxsplit=1)
-            f.seek(int(offset))
-            f.write(bytes(rplstr, encoding=encmap[enc]))
+            (offset, rplstr) = entry.lstrip().split(" ", maxsplit=1)
+            
+            ioffset = int(offset.strip())
+            f.seek(ioffset,0)
+                    
+            bytestr = bytes(rplstr, p_enc)
+            targetbytes = f.read(len(bytestr))
+            if bytestr != targetbytes:
+                click.echo("Patched at %d: %s -> %s"%(ioffset, targetbytes, bytestr))
+            f.seek(int(offset.strip()),0)
+            f.write(bytestr)
     
-    results = subprocess.check_output(["strings", "-a", "-t", "d", "-e", enc, filepath])
-    click.echo("# New strings output")
-    click.echo("# ==================")
-    sys.stdout.write(str(results, encoding="ascii"))
+#    results = subprocess.check_output(["strings", "-a", "-t", "d", "-e", enc, filepath])
+#    click.echo("# New strings output")
+#    click.echo("# ==================")
+#    sys.stdout.write(str(results, encoding="ascii"))
 
 
 
-
+    
 if __name__ == "__main__":
     cli()
